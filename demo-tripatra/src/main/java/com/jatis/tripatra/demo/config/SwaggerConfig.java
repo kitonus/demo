43bb55b11071f4a8a0bec3d9f77f37d7529f6fb8
Hanendyo Wicaksono <kitonus@gamil.com>
2019-10-28 23:27:56 +0700

    Add Swagger host

--- demo-tripatra/src/main/java/com/jatis/tripatra/demo/config/SwaggerConfig.java
+++ demo-tripatra/src/main/java/com/jatis/tripatra/demo/config/SwaggerConfig.java
@@ -1,5 +1,11 @@
 package com.jatis.tripatra.demo.config;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,14 +18,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	private static final Logger log = LoggerFactory.getLogger(SwaggerConfig.class);
 
-	@Bean
-	public Docket api() {                
-	    return new Docket(DocumentationType.SWAGGER_2)          
-	      .select()            
-	      //.apis(RequestHandlerSelectors.basePackage("com.jatis.demo.demoapi"))
-	      .apis(RequestHandlerSelectors.any())
-	      .paths(PathSelectors.any())
-	      .build();
-	}
+    @Bean
+    public Docket api(@Value("${service.swagger-host:N/A}") String host) {
+        log.info("--=-->>service.swagger-host="+host);
+        if (host != null && !"N/A".equals(host)) {
+                return new Docket(DocumentationType.SWAGGER_2)
+                        .protocols(new HashSet<String>(Arrays.asList("http","https")))
+                        .host(host)
+                .select()
+                .apis(RequestHandlerSelectors
+                                .any())
+                .paths(PathSelectors.any())
+                .build();
+        } else {
+                return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors
+                                .any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+    }
 }
